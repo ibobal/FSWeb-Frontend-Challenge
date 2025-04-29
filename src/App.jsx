da77faa69d099dc794a1dee584e243260a0c7072
ibobal <119145325+ibobal@users.noreply.github.com>
2025-04-29 18:54:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <Header />
     </>
   );
 }
