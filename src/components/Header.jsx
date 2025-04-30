dc95247f4388a5414129878bcd2ec7cc2de5d55c
ibobal <119145325+ibobal@users.noreply.github.com>
2025-04-30 17:00:42 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,18 +7,25 @@ export default function Header() {
   const dispatch = useDispatch();
 
   return (
-    <header className="flex items-center justify-between px-70 py-8 bg-customIndigo">
-      <div className="text-2xl font-bold">ibobal</div>
+    <header className="flex items-center justify-between py-8">
+      <div className="text-2xl text-customLime font-bold">ibobal</div>
       <div className="flex items-center space-x-6">
         <button
-          className="text-sm text-lime-300 font-medium hover:bg-gray-400 p-2 rounded-full"
+          className="text-sm text-customLime font-medium hover:bg-gray-400 p-2 rounded-full"
           onClick={() => dispatch(setLanguage())}
         >
           {language === "en" ? "TÜRKÇE'YE GEÇ" : "SWITCH TO ENGLISH"}
         </button>
-        <button onClick={() => dispatch(setTheme())}>
-          {theme === "dark" ? "DARK MODE" : "LIGHT MODE"}
-        </button>
+        <label className="flex cursor-pointer gap-2">
+          <input
+            type="checkbox"
+            className="toggle theme-controller"
+            onClick={() => dispatch(setTheme())}
+          />
+          <span className="label-text">
+            {theme === "dark" ? "DARK MODE" : "LIGHT MODE"}
+          </span>
+        </label>
       </div>
     </header>
   );
