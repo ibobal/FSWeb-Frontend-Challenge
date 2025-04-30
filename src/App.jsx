a9a56dc4581e6c0099465e271abaaa53170ac85f
ibobal <119145325+ibobal@users.noreply.github.com>
2025-04-30 18:55:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
 
 function App() {
-
   return (
     <>
       <Header />
       <Hero />
+      <Skills />
     </>
   );
 }
