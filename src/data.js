fb707e3792374dcc5f86f1367e8f4e45e7c1bbd8
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 00:30:18 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,158 @@
+export const data = {
+  en: {
+    heroSection: {
+      title: "I am a Frontend Developer...",
+      subtitle:
+        "who likes to craft solid and scalable products with great user experiences.",
+      image: "",
+    },
+    skillsSection: {
+      title: "Skills",
+      skills: [
+        {
+          name: "JAVASCRIPT",
+          icon: "javaScript",
+        },
+        {
+          name: "NODE",
+          icon: "Node.js",
+        },
+        {
+          name: "REACT",
+          icon: "react",
+        },
+        {
+          name: "VS CODE",
+          icon: "react",
+        },
+        {
+          name: "REDUX",
+          icon: "react",
+        },
+        {
+          name: "Figma",
+          icon: "figma",
+        },
+      ],
+    },
+    profileSection: {
+      title: "Profile",
+      image: "",
+      infoSection: {
+        title: "Basic Information",
+        birthDate: "Date Of Birth",
+        cityOfResidence: "City Of Residence",
+        education: "Email",
+        role: "Preferred Role",
+      },
+      aboutMeSection: {
+        title: "About Me",
+        descFirst:
+          "Hello, I am İbrahim. I graduated from Gazi University, Department of Business Administration in 2020. After my graduation, I decided to take a leave of absence due to my interest in the software field.",
+        descSecond: "I continue my career as a Frontend UI Developer.",
+      },
+    },
+    projectsSection: {
+      title: "Projects",
+      projects: [
+        {
+          name: "Workintech",
+          image: "",
+          desc: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          skills: ["react", "redux", "vercel"],
+          link: "View Site",
+        },
+        {
+          name: "Journey",
+          image: "",
+          desc: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          skills: ["react", "redux", "vercel"],
+          link: "View Site",
+        },
+      ],
+    },
+    footerSection: {
+      title: "Send me a message",
+      desc: "Got a question or proposal, or just want to say hello? Go ahead.",
+      email: "ibrahimballi454@gmail.com",
+    },
+  },
+  tr: {
+    heroSection: {
+      title: "Ben bir Frontend Geliştiricisiyim...",
+      subtitle:
+        "sağlam ve ölçeklenebilir ürünler geliştirip, harika kullanıcı deneyimleri sunmayı seviyorum.",
+      image: "",
+    },
+    skillsSection: {
+      title: "Yetenekler",
+      skills: [
+        {
+          name: "JAVASCRIPT",
+          icon: "javaScript",
+        },
+        {
+          name: "NODE",
+          icon: "Node.js",
+        },
+        {
+          name: "REACT",
+          icon: "react",
+        },
+        {
+          name: "VS CODE",
+          icon: "react",
+        },
+        {
+          name: "REDUX",
+          icon: "react",
+        },
+        {
+          name: "Figma",
+          icon: "figma",
+        },
+      ],
+    },
+    profileSection: {
+      title: "Profil",
+      image: "",
+      infoSection: {
+        title: "Temel Bilgiler",
+        birthDate: "Doğum Tarihi",
+        cityOfResidence: "İkamet Şehri",
+        education: "E-posta",
+        role: "Tercih Edilen Rol",
+      },
+      aboutMeSection: {
+        title: "Hakkımda",
+        descFirst:
+          "Merhaba, ben İbrahim. 2020 yılında Gazi Üniversitesi İşletme Bölümü'nden mezun oldum. Mezuniyetimin ardından yazılım alanına olan ilgimden dolayı bu alanda kendimi geliştirmeye karar verdim.",
+        descSecond: "Kariyerime Frontend UI Developer olarak devam ediyorum.",
+      },
+    },
+    projectsSection: {
+      title: "Projeler",
+      projects: [
+        {
+          name: "Workintech",
+          image: "",
+          desc: "Kullanıcıların hangi çerezleri kabul edip etmeyeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimal kurulumlu bir çerez eklentisi. Vanilla JS, SCSS ve Parcel Bundler ile geliştirildi. NPM paketi olarak mevcuttur ve Git deposu sayesinde tema ve kod üzerinde her türlü özelleştirme yapılabilir.",
+          skills: ["react", "redux", "vercel"],
+          link: "Siteyi Görüntüle",
+        },
+        {
+          name: "Journey",
+          image: "",
+          desc: "Kullanıcıların hangi çerezleri kabul edip etmeyeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimal kurulumlu bir çerez eklentisi. Vanilla JS, SCSS ve Parcel Bundler ile geliştirildi. NPM paketi olarak mevcuttur ve Git deposu sayesinde tema ve kod üzerinde her türlü özelleştirme yapılabilir.",
+          skills: ["react", "redux", "vercel"],
+          link: "Siteyi Görüntüle",
+        },
+      ],
+    },
+    footerSection: {
+      title: "Bana bir mesaj gönder",
+      desc: "Bir sorunuz, teklifiniz mi var ya da sadece merhaba mı demek istiyorsunuz? Devam edin.",
+      email: "ibrahimballi454@gmail.com",
+    },
+  },
+};
