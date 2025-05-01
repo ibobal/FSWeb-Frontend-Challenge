5709be3588abe6f523db6191b073e851d78cec39
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 01:00:19 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
+import { useSelector } from "react-redux";
+
 export default function Hero() {
+  const { data, loading, error } = useSelector((state) => state.data);
+  const { title, subtitle, image } = data.heroSection;
+
   return (
     <div className="flex justify-between min-h-screen bg-[linear-gradient(to_right,_#4b37cf_0%,_#4b37cf_67%,_#c9f080_50%,_#c9f080_100%)] px-20 md:px-40 lg:px-80 py-16 ">
       <div className="grow-1 flex flex-col justify-center items-start text-left">
-        <h1 className="text-5xl text-customLime font-bold">
-          I am a Frontend Developer...
-        </h1>
-        <p className="text-white py-6">
-          ...who likes to craft solid and scalable frontend products with great
-          user experiences.
-        </p>
+        <h1 className="text-5xl text-customLime font-bold">{title}</h1>
+        <p className="text-white py-6">{subtitle}</p>
         <div className="join gap-2">
           <button className="join-item btn btn-white text-customPurple mt-4">
             Github
@@ -19,10 +19,7 @@ export default function Hero() {
         </div>
       </div>
       <div className="flex justify-center items-center">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
+        <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
       </div>
     </div>
   );
