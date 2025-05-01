6b70c1f0c5505c36e37d12ab61e165fc9514e5e3
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 01:00:21 +0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,53 +1,22 @@
+import { useSelector } from "react-redux";
+
 export default function Skills() {
+  const { data, loading, error } = useSelector((state) => state.data);
+  const { title, skills } = data.skillsSection;
+
   return (
     <div className="flex justify-between px-20 md:px-40 lg:px-80 py-16 ">
       <div>
-        <h1 className="text-5xl text-customPurple font-bold">Skills</h1>
+        <h1 className="text-5xl text-customPurple font-bold">{title}</h1>
       </div>
       <div className="grow-1 flex justify-end items-center">
         <div className="grid grid-cols-2 gap-4 mt-8">
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/color/96/000000/javascript--v1.png"
-              alt="JavaScript"
-            />
-            <p className="text-center">JAVASCRIPT</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/color/96/000000/nodejs.png"
-              alt="Node JS"
-            />
-            <p className="text-center">NODE</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/color/96/000000/react-native.png"
-              alt="React"
-            />
-            <p className="text-center">REACT</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/?size=96&id=i19Ns28h30P4&format=png&color=000000"
-              alt="VS Code"
-            />
-            <p className="text-center">VS CODE</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/color/96/000000/redux.png"
-              alt="Redux"
-            />
-            <p className="text-center">REDUX</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/color/96/000000/figma.png"
-              alt="Figma"
-            />
-            <p className="text-center">FIGMA</p>
-          </div>
+          {skills.map((skill, index) => (
+            <div key={index} className="flex items-center">
+              <img src={skill.icon} alt={skill.name} className="w-16 h-16" />
+              <p className="text-center">{skill.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
