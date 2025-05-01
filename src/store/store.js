665748a930ae551b0b0bd205392273649008b8a0
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 00:30:26 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "./reducers/settingsSlice";
+import dataReducer from "./reducers/dataSlice";
 
 const store = configureStore({
   reducer: {
     settings: settingsReducer,
+    data: dataReducer,
   },
 });
 
