b76d6e0ef11d2cd65daacc290c35867e0c93e43e
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 00:31:02 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
-    <div class="flex justify-between min-h-screen bg-[linear-gradient(to_right,_#4b37cf_0%,_#4b37cf_67%,_#c9f080_50%,_#c9f080_100%)] px-20 md:px-40 lg:px-80 py-16 ">
+    <div className="flex justify-between min-h-screen bg-[linear-gradient(to_right,_#4b37cf_0%,_#4b37cf_67%,_#c9f080_50%,_#c9f080_100%)] px-20 md:px-40 lg:px-80 py-16 ">
       <div className="grow-1 flex flex-col justify-center items-start text-left">
-        <h1 class="text-5xl text-customLime font-bold">
+        <h1 className="text-5xl text-customLime font-bold">
           I am a Frontend Developer...
         </h1>
-        <p class="text-white py-6">
+        <p className="text-white py-6">
           ...who likes to craft solid and scalable frontend products with great
           user experiences.
         </p>
@@ -21,7 +21,7 @@ export default function Hero() {
       <div className="flex justify-center items-center">
         <img
           src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          class="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-sm rounded-lg shadow-2xl"
         />
       </div>
     </div>
