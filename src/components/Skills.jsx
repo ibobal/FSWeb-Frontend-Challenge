9b9031c5bd4d8ac8f4770cf26be5c7c7c7fc72f6
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 03:17:02 +0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,19 +5,19 @@ export default function Skills() {
   const { title, skills } = data.skillsSection;
 
   return (
-    <div className="flex justify-between px-20 md:px-40 lg:px-80 py-16 ">
-      <div>
-        <h1 className="text-5xl text-customPurple font-bold">{title}</h1>
-      </div>
-      <div className="grow-1 flex justify-end items-center">
-        <div className="grid grid-cols-2 gap-4 mt-8">
-          {skills.map((skill, index) => (
-            <div key={index} className="flex items-center">
-              <img src={skill.icon} alt={skill.name} className="w-16 h-16" />
-              <p className="text-center">{skill.name}</p>
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col md:flex-row justify-between px-6 md:px-20 lg:px-40 py-16 dark:bg-[#252128]">
+      <h1 className="text-3xl md:text-5xl text-customPurple dark:text-customLime font-bold mb-8 md:mb-0">
+        {title}
+      </h1>
+      <div className="grid grid-cols-2 sm:grid-cols-2 gap-y-4 gap-x-10 md:gap-x-60">
+        {skills.map((skill, index) => (
+          <div key={index} className="flex items-center gap-4">
+            <img src={skill.icon} alt={skill.name} className="w-16 md:w-32" />
+            <p className="text-center text-gray-500 dark:text-white text-lg md:text-2xl">
+              {skill.name}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
