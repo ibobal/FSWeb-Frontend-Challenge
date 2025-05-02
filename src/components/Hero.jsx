e4e84d641e8ec1fee56bf86d4b4acd43b2a4243c
ibobal <119145325+ibobal@users.noreply.github.com>
2025-05-02 03:17:07 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,39 @@
 import { useSelector } from "react-redux";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 export default function Hero() {
   const { data, loading, error } = useSelector((state) => state.data);
   const { title, subtitle, image } = data.heroSection;
 
   return (
-    <div className="flex justify-between min-h-screen bg-[linear-gradient(to_right,_#4b37cf_0%,_#4b37cf_67%,_#c9f080_50%,_#c9f080_100%)] px-20 md:px-40 lg:px-80 py-16 ">
-      <div className="grow-1 flex flex-col justify-center items-start text-left">
-        <h1 className="text-5xl text-customLime font-bold">{title}</h1>
-        <p className="text-white py-6">{subtitle}</p>
-        <div className="join gap-2">
-          <button className="join-item btn btn-white text-customPurple mt-4">
+    <div className="flex flex-col md:flex-row justify-around md:justify-between min-h-screen bg-customIndigo dark:bg-[#171043] md:bg-[linear-gradient(to_right,_#4b37cf_0%,_#4b37cf_82%,_#c9f080_50%,_#c9f080_100%)] md:dark:bg-[linear-gradient(to_right,_#171043_0%,_#171043_82%,_#211F0B_50%,_#211F0B_100%)] px-6 md:px-20 lg:px-40 py-16">
+      <div className="flex flex-col justify-center items-start text-left mb-8 md:mb-0">
+        <h1 className="text-3xl md:text-5xl text-customLime font-bold">
+          {title}
+        </h1>
+        <p className="text-white text-lg md:text-3xl py-6 w-full md:w-150">
+          {subtitle}
+        </p>
+        <div className="flex gap-4">
+          <button className="btn btn-white text-lg text-customPurple dark:text-white dark:outline mt-4">
+            <span className="text-2xl">
+              <FaGithub />
+            </span>
             Github
           </button>
-          <button className="join-item btn btn-white text-customPurple mt-4">
+          <button className="btn btn-white text-lg text-customPurple dark:text-white dark:outline mt-4">
+            <span className="text-2xl">
+              <FaLinkedinIn />
+            </span>
             LinkedIn
           </button>
         </div>
       </div>
       <div className="flex justify-center items-center">
-        <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
+        <img
+          src={image}
+          className="max-w-full md:max-w-sm rounded-lg shadow-2xl"
+        />
       </div>
     </div>
   );
